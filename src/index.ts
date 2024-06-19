7879bc0ad91f08e6dec5a650add4c31b38b8eb4a
Daniil Litvinov <danlitw@ya.ru>
2024-06-20 00:22:15 +0500

    fix: правка билда

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,12 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV !== "development") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV !== "development") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 server.listen(PORT, () => {
 	console.log("Server is running on port " + PORT);
